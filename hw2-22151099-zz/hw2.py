58fb236e812b464ae4aced823bd97312bcb3e914
125929280 <37729005+125929280@users.noreply.github.com>
2021-10-12 14:49:54 +0800

    /

--- hw2-22151099-zz/hw2.py
+++ hw2-22151099-zz/hw2.py
@@ -3,8 +3,10 @@ import hashlib
 import binascii
 import urllib.request
 import json
+import os
 
-target = '0000000000000000000022d8f11190d40de785dbf24bdfaa7d4040c804c5e507'
+# target = '0000000000000000000022d8f11190d40de785dbf24bdfaa7d4040c804c5e507'
+target = input('请输入需要验证的区块hash : ')
 
 """
 验证区块哈希的正确性
@@ -32,6 +34,7 @@ json_record = get_json_record(target)
 
 # 展示区块信息
 def show_info():
+    print('区块信息 : ')
     print('hash : ' + json_record['hash'])
     print('ver : ' + str(json_record['ver']))
     print('prev_block : ' + json_record['prev_block'])
@@ -39,6 +42,7 @@ def show_info():
     print('time : ' + str(json_record['time']))
     print('bits : ' + str(json_record['bits']))
     print('nonce : ' + str(json_record['nonce']))
+    print('')
 
 # 计算区块哈希
 def cal_hash():
@@ -53,11 +57,13 @@ def validate_hash():
     show_info()
     calculated_hash = cal_hash()
     print('calculated_hash = ' + calculated_hash)
-
+    print('')
+    print('block hash 验证结果 : ')
     if calculated_hash == target:
         print('block hash is right')
     else:
         print('block hash is wrong')
+    print('===========================================================================')
 
 """
 验证merkle_root正确性
@@ -71,14 +77,17 @@ def cal_merkle_root():
     json_tx_record = json_record['tx']
     tx_nums = len(json_record['tx'])
     tx_hash = []
+    print('开始读入交易hash ...')
     for i in range(tx_nums):
         if i % 100 == 0:
             print('reading %d hashs...'%i)
         tx_hash.append(big_small_end_trans(json_tx_record[i]['hash']))
-
+    print('交易hash读取完毕 ...')
+    print('')
     if tx_nums == 1:
         return tx_hash[0]
 
+    print('开始计算merkle_tree内部各节点hash ...')
     while tx_nums != 1:
         index = 0
         new_index = 0
@@ -96,16 +105,22 @@ def cal_merkle_root():
         tx_hash = tx_hash[:new_index]
         tx_nums = new_index
         print('当前层的节点数量' + str(tx_nums))
+    print('merkle_tree各节点hash计算完毕 ...')
+    print('')
     return tx_hash[0]
 
 # 验证merkle_root
 def validate_merkle_root():
     calculated_merkle_root = big_small_end_trans(cal_merkle_root())
     print('calculated_merkle_root = ' + calculated_merkle_root)
+    print('')
+    print('merkle_root 验证结果 : ')
     if calculated_merkle_root == json_record['mrkl_root']:
         print('merkle root is right')
     else:
         print('merkle root is wrong')
+    print('===========================================================================')
 
 validate_hash()
 validate_merkle_root()
+os.system('pause')
